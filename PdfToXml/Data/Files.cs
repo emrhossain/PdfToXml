070c6ccdb137423b9d2f3e6d5268c961b9838de9
A-Abuowda <aa97@live.no>
2023-02-09 07:56:26 +0600

    Add project files.

---
+++ PdfToXml/Data/Files.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PdfToXml.Data
+{
+    public class Files
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        public string PdfUrl { get; set; }
+
+    }
+}
---
+++ PdfToXml/Data/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace PdfToXml.Data
+{
+    public class User: IdentityUser
+    {
+        [PersonalData]
+        [MaxLength(64)]
+        public string Name { get; set; }
+        [PersonalData]
+        [MaxLength(2000)]
+        public string About { get; set; }
+
+    }
+}
